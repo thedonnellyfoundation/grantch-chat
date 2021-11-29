aab41212119b17b14adef4e1ebc0637a05de78e7
thedonnellyfoundation <mycolonistight@gmail.com>
2021-11-29 14:21:27 +0800

    cringe

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const {Server} = require('socket.io');
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+io.on('connection', (socket) => {
+    console.log('a user has connected');
+
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
+        io.emit('chat message', msg);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('a user has disconnected');
+    });
+});
+
+server.listen(80, ()=> {
+    console.log('listening on *:80');
+});
